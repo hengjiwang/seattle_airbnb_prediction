b08b5a6d58f8b408600cce4c9cfcb3eedb336cf5
hengjiwang <hengjw@uw.edu>
2019-05-06 22:55:02 -0700

    print date

--- clean_and_feature_extraction/python/create_dataset.py
+++ clean_and_feature_extraction/python/create_dataset.py
@@ -34,5 +34,5 @@ if __name__ == '__main__':
     sub_lists = os.listdir(sys.argv[1])
     for sub in sub_lists:
         creater = CreateDataSet(sys.argv[1]+sub+'/', sys.argv[2]+'/'+sub+'.csv')
-        print(sub)
+        print('Processing'+sub+'...')
         creater.create()
